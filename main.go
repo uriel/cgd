ee32d7ad3116c6c71ccf63c51001598f23935574
Uriel <u@berlinblue.org>
2012-09-16 06:15:31 +0200

    Simplification: don't wrap cgi handler in a FuncHandler.
    Other minor simplifications and error reporting cleanups.

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
 	"os"
 
@@ -18,28 +17,6 @@ var serveFcgi = flag.Bool("f", false, "Run as a FCGI 'server' instead of HTTP")
 var debug = flag.Bool("debug", false, "Print debug msgs to stderr.")
 var address = flag.String("a", ":3333", "Listen address")
 
-func handleCgi(res http.ResponseWriter, req *http.Request) {
-	c := *cmd
-	if c[0] != "/"[0] {
-		c = "./" + c
-	}
-
-	os.Setenv("PATH", os.Getenv("PATH")+":.")
-
-	h := cgi.Handler{
-		Path:       c,
-		Root:       "/",
-		Dir:        *pwd,
-		InheritEnv: []string{"PATH", "PLAN9"},
-	}
-
-	if *debug {
-		fmt.Fprintf(os.Stderr, "%v", h)
-	}
-
-	h.ServeHTTP(res, req)
-}
-
 func main() {
 	flag.Usage = usage
 	flag.Parse()
@@ -48,31 +25,40 @@ func main() {
 		usage()
 	}
 
-	h := http.HandlerFunc(handleCgi)
+	// This is a hack to make p9p's rc happier for some unknown reason.
+	c := *cmd
+	if c[0] != "/"[0] {
+		c = "./" + c
+	}
 
+	os.Setenv("PATH", os.Getenv("PATH")+":.")
+
+	h := &cgi.Handler{
+		Path:       c,
+		Root:       "/",
+		Dir:        *pwd,
+		InheritEnv: []string{"PATH", "PLAN9"},
+	}
+
+	var err error
 	if *serveFcgi {
-		l, err := net.Listen("tcp", *address)
-		if err != nil {
-			log.Fatal(err)
+		if l, err := net.Listen("tcp", *address); err == nil {
+			log.Println("Starting FastCGI daemon listening on", *address)
+			err = fcgi.Serve(l, h)
 		}
-
-		log.Println("Starting FastCGI daemon listening on", *address)
-		fcgi.Serve(l, h)
 
 	} else {
-		http.Handle("/", h)
-
 		log.Println("Starting HTTP server listening on", *address)
-		err := http.ListenAndServe(*address, nil)
-		if err != nil {
-			log.Fatal(err)
-		}
+		err = http.ListenAndServe(*address, h)
+	}
+
+	if err != nil {
+		log.Fatal(err)
 	}
 }
 
 func usage() {
-	fmt.Fprintf(os.Stderr, "usage: cgd [-s] -c prog [-w wdir] [-a addr]")
-
+	os.Stderr.WriteString("usage: cgd [-s] -c prog [-w wdir] [-a addr]\n")
 	flag.PrintDefaults()
 	os.Exit(2)
 }
